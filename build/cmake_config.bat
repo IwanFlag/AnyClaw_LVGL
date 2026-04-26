186c8388bad4ca809658bd4099f188524cbaa2b5
thundersoft-blockfighter <2443150429@qq.com>
2026-04-26 16:11:14 +0800

    chore(repo): clean generated build artifacts and stale resources

--- build/cmake_config.bat
+++
@@ -1,4 +0,0 @@
-﻿@echo off
-call "C:\VSBuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-set PATH=%PATH%;C:\Program Files\CMake\bin
-"C:\Program Files\CMake\bin\cmake.exe" -G "NMake Makefiles" -B "D:\workspace\AnyClaw\AnyClaw_LVGL_Code\AnyClaw_LVGL\build" -DCMAKE_BUILD_TYPE=Release "D:\workspace\AnyClaw\AnyClaw_LVGL_Code\AnyClaw_LVGL"
